8834a17860ed5dce85e31cfa8f5288c42ac259b9
Varun Kumar <varun.kumar12aug@gmail.com>
2025-08-26 01:01:00 +0530

    v1 version

---
+++ app/src/main/java/com/k8s/learning/controller/AppController.java
@@ -0,0 +1,16 @@
+package com.k8s.learning.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    public ResponseEntity<?> get(){
+        return ResponseEntity.ok("Response from <h1>V1 version</h1> APP");
+    }
+}
